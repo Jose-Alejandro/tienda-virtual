cdaedc0109735f2abde24a5882ea028106e284c8
alejandro <jose.alejandro.esquivel@gmail.com>
2021-07-04 13:40:59 -0500

    Add cart Template
    Co-authored-by: Guillermo-alt <Guillermo-alt@users.noreply.github.com>

--- assets/Cart.js
+++ assets/Cart.js
@@ -1,5 +1,48 @@
-class Cart() {
+class Cart(user) {
     constructor() {
-
+        let total = 0;
     }
+
+    AddProduct(product) {
+        this.products.push(product);
+    };
+
+    RemoveProduct(id) {
+        this.products.remove(id);
+    };
+
+    Buy() {
+        checkCreditCard();
+        applyCoupon();
+        pay();
+    };
+
+    getTotal() {};
+    applyCoupon() {};
+    checkCreditCard() {};
+    pay() {};
+}
+
+class Productos() {
+    constructor() {
+        this.id = 0;
+        this.name = ''
+        this.quantity = 0;
+        this.price = 0;
+        this.thumnail = 0;
+        this.description = '';
+    };
+    CreateProduct() {
+        storeToDB(this);
+    };
+    UpdateProduct(id, product) {
+        UpdateToDB(id, product);
+    };
+
+    DeleteProduct() {
+        deleteFromDB(this.id);
+    };
+    RetrieveProduct() {
+        retrieveDB(this.id);
+    };
 }
