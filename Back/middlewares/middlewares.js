3551ed9da8f14e006de752bdf7ff933263b2c3eb
alejandro <jose.alejandro.esquivel@gmail.com>
2021-07-18 16:47:43 -0500

    add white list env for cors and rate limiter midd

---
+++ Back/middlewares/middlewares.js
@@ -0,0 +1,24 @@
+/** Import modules */
+const rateLimit = require("express-rate-limit");
+
+
+/** Rate limiter middleware */
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	message: 'Requests exceeded, wait 25 minutes'
+});
+
+/** cors option middleware for whitelist*/
+
+const corsOption = {
+	origin: function (origin, callback) {
+		if (process.env.WHITE_LIST.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not authorized by CORS'));
+		}
+	}
+};
+
+module.exports = { corsOption, limiter };
