0bbf03573a766f9ec816ec87704734c00b116a75
alejandro <jose.alejandro.esquivel@gmail.com>
2021-07-31 17:00:19 -0500

    add files

--- Back/app/view/mercado-libre.routes.js
+++ Back/app/view/mercado-libre.routes.js
@@ -15,7 +15,7 @@ module.exports = (app) => {
             res.status(400).json("Call to Mercado Libre Failed, try later" + err);
         }
     });
-    app.get('/product',cors(middlewares.corsOption) ,async (req, res) => {
+    app.get('/product', cors(middlewares.corsOption), async (req, res) => {
         try {
             let result = await fetch(`https://api.mercadolibre.com/items/${req.query.q}`);
             let response = await result.json();
