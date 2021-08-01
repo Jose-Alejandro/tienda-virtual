d9dfde42fa72c64900b89088803bdaae071e1fa6
alejandro <jose.alejandro.esquivel@gmail.com>
2021-08-01 15:48:35 -0500

    add conection to dataBase

--- Back/server.js
+++ Back/server.js
@@ -6,17 +6,33 @@ const cors = require('cors');
 require('dotenv').config();
 const middlewares = require('./middlewares/middlewares');
 const mercadolibreRoutes = require('./app/view/mercado-libre.routes');
+const viewUsers = require('./app/view/view.Users');
+const db = require('./db/db.conection');
+
 
 /** Global middlewares */
 app.use(cors());
 app.use(express.json());
 app.use(middlewares.limiter);
 
-/** Create server */
-app.listen(process.env.PORT, process.env.HOST, () => {
-    console.log(`Server started at http://${process.env.HOST}:${process.env.PORT}`);
-});
 
+/** Start server */
+async function startServer() {
+	try {
+		// await users.sync();
+		await db.authenticate();
+		console.log('Conected to Database');
+		app.listen(process.env.PORT, process.env.HOST, () => {
+			console.log(`Server started at http://${process.env.HOST}:${process.env.PORT}`);
+		});
+	} catch (error) {
+		console.error('No se pudo conectar correctamebte con la Base de datos:', error);
+	}
+}
+
+startServer();
 
 /** Start API routes */
 mercadolibreRoutes(app);
+
+viewUsers(app);
