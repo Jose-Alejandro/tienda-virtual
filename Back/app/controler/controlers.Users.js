e268b0db7a34eca76f8e5542d203bcd262d4b8c8
alejandro <jose.alejandro.esquivel@gmail.com>
2021-08-01 20:47:02 -0500

    add login JWT

--- Back/app/controler/controlers.Users.js
+++ Back/app/controler/controlers.Users.js
@@ -1,4 +1,5 @@
 const modelsUsers = require('../model/models.Users');
+const jwt = require('jsonwebtoken');
 
 module.exports.registerUser = async (newUser) => {
 	try {
@@ -9,4 +10,32 @@ module.exports.registerUser = async (newUser) => {
 	} catch (error) {
 		throw error;
 	}
+};
+
+module.exports.validateUser = async (usr) => {
+	try {
+		if (await modelsUsers.UserExists(usr)) {
+			return true;
+		} else {
+			return false;
+		}
+	} catch (error) {
+		console.log(error.message);
+		throw error;
+	}
+};
+
+module.exports.generateUserToken = async (user) => {
+	const token = jwt.sign(
+		{ data: user },
+		process.env.SECRET_KEY
+	);
+	return token;
+};
+
+module.exports.verifyUserToken = async (token) => {
+	if (jwt.verify(token, process.env.SECRET_KEY)) {
+		return true;
+	}
+	throw new Error('Invalid authentication, try to login again');
 };
