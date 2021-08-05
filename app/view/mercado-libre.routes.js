6ca60c21c97963f827ddbfa58d0dff8030f974f6
alejandro <jose.alejandro.esquivel@gmail.com>
2021-08-05 17:19:23 -0500

    change json for send

--- app/view/mercado-libre.routes.js
+++ app/view/mercado-libre.routes.js
@@ -5,24 +5,24 @@ const cors = require('cors');
 
 
 module.exports = (app) => {
-    app.get('/trendProducts',/* cors(middlewares.corsOption),*/ async (req, res) => {
-        try {
-            let result = await fetch(`https://api.mercadolibre.com/sites/MLM/search?q=${req.query.q}&limit=10`);
-            let response = await result.json();
-            res.send(response);
-        } catch (err) {
-            console.log(err);
-            res.status(400).json("Call to Mercado Libre Failed, try later" + err);
-        }
-    });
-    app.get('/product', /*cors(middlewares.corsOption),*/ async (req, res) => {
-        try {
-            let result = await fetch(`https://api.mercadolibre.com/items/${req.query.q}`);
-            let response = await result.json();
-            res.send(response);
-        } catch (err) {
-            console.log(err);
-            res.status(400).json("Call to Mercado Libre Failed, try later" + err);
-        }
-    });
+	app.get('/trendProducts', /* cors(middlewares.corsOption), */ async (req, res) => {
+		try {
+			let result = await fetch(`https://api.mercadolibre.com/sites/MLM/search?q=${req.query.q}&limit=10`);
+			let response = await result.json();
+			res.send(response);
+		} catch (err) {
+			console.log('error: ' + err.message);
+			res.status(400).send("Call to Mercado Libre Failed, try later" + err.message);
+		}
+	});
+	app.get('/product', /*cors(middlewares.corsOption),*/ async (req, res) => {
+		try {
+			let result = await fetch(`https://api.mercadolibre.com/items/${req.query.q}`);
+			let response = await result.json();
+			res.send(response);
+		} catch (err) {
+			console.log(err);
+			res.status(400).send("Call to Mercado Libre Failed, try later" + err);
+		}
+	});
 };
--- public/assets/modulos/index.js
+++ public/assets/modulos/index.js
@@ -10,109 +10,105 @@ const ML_ENDPOINT = 'trendProducts';
 
 /** Clase Store */
 class Store {
-    constructor(key) {
-        this.word = key;
-    }
+	constructor(key) {
+		this.word = key;
+	}
 
-    createTrendElement(product) {
-        let productContainer = document.getElementById("product-container");
+	createTrendElement(product) {
+		let productContainer = document.getElementById("product-container");
 
-        let row = document.createElement("div");
-        row.classList.add("row");
-        row.classList.add("featurette");
+		let row = document.createElement("div");
+		row.classList.add("row");
+		row.classList.add("featurette");
 
-        let divider = document.createElement("hr");
-        divider.classList.add("featurette-divider");
+		let divider = document.createElement("hr");
+		divider.classList.add("featurette-divider");
 
-        let headerDiv = document.createElement("div");
-        headerDiv.classList.add("col-md-7");
+		let headerDiv = document.createElement("div");
+		headerDiv.classList.add("col-md-7");
 
-        let productHeader = document.createElement("h2");
-        productHeader.classList.add("featurette-heading");
-        productHeader.textContent = product.title;
+		let productHeader = document.createElement("h2");
+		productHeader.classList.add("featurette-heading");
+		productHeader.textContent = product.title;
 
-        let itemPrice = document.createElement('p');
-        itemPrice.classList.add('lead');
-        itemPrice.textContent = Intl.NumberFormat('en-EN', { style: "currency", currency: "MXN", }).format(product.price);
+		let itemPrice = document.createElement('p');
+		itemPrice.classList.add('lead');
+		itemPrice.textContent = Intl.NumberFormat('en-EN', { style: "currency", currency: "MXN", }).format(product.price);
 
-        let imageContainer = document.createElement("div");
-        imageContainer.classList.add("col-md-5");
+		let imageContainer = document.createElement("div");
+		imageContainer.classList.add("col-md-5");
 
-        let itemImage = document.createElement("img");
-        itemImage.setAttribute("src", product.thumbnail);
-        itemImage.style.width = "300px";
-        itemImage.style.height = "300px";
+		let itemImage = document.createElement("img");
+		itemImage.setAttribute("src", product.thumbnail);
+		itemImage.style.width = "300px";
+		itemImage.style.height = "300px";
 
-        let addButton = document.createElement("button");
-        addButton.setAttribute("id","addToCar");
-        addButton.setAttribute("onclick",`clickAdd('${product.id}')`);
-        addButton.classList.add("btn");
-        addButton.classList.add("btn-primary");
-        addButton.textContent = 'Add to cart';
+		let addButton = document.createElement("button");
+		addButton.setAttribute("id", "addToCar");
+		addButton.setAttribute("onclick", `clickAdd('${product.id}')`);
+		addButton.classList.add("btn");
+		addButton.classList.add("btn-primary");
+		addButton.textContent = 'Add to cart';
 
-        productContainer.appendChild(divider);
-        productContainer.appendChild(row);
-        row.appendChild(headerDiv);
-        headerDiv.appendChild(productHeader);
-        headerDiv.appendChild(itemPrice);
-        row.appendChild(imageContainer);
-        imageContainer.appendChild(itemImage);
-        imageContainer.appendChild(addButton);
-    }
-    /** funcion que usa la palabras claves para buscar y traer productos */
-    async getProductsFromKeyword(word) {
-        try {
-            const response = await fetch(`http://127.0.0.1:3000/${ML_ENDPOINT}?q=${word}`);
+		productContainer.appendChild(divider);
+		productContainer.appendChild(row);
+		row.appendChild(headerDiv);
+		headerDiv.appendChild(productHeader);
+		headerDiv.appendChild(itemPrice);
+		row.appendChild(imageContainer);
+		imageContainer.appendChild(itemImage);
+		imageContainer.appendChild(addButton);
+	}
+	/** funcion que usa la palabras claves para buscar y traer productos */
+	async getProductsFromKeyword(word) {
+		try {
+			const response = await fetch(`http://127.0.0.1:3000/${ML_ENDPOINT}?q=${word}`);
+			const products = await response.json(); //promesa en espera
+			return products;
+		} catch (error) {
+			throw error;
+		}
 
-            const products = await response.json(); //promesa en espera
-            return products;
-        } catch (error) {
-            throw new Error(error);
-        }
+	}
 
-    }
+	/** trae productos por palabra clave */
+	async getTrendProducts() {
+		let productContainer = document.getElementById("product-container");
+		productContainer.innerHTML = '';
 
-    /** trae productos por palabra clave */
-    async getTrendProducts() {
-        let productContainer = document.getElementById("product-container");
-        productContainer.innerHTML = '';
+		try {
+			this.CategoryProducts = await this.getProductsFromKeyword(this.word); //palabras claves
+			for (let product in this.CategoryProducts.results) {
+				this.createTrendElement(this.CategoryProducts.results[product]);
+			}
+		} catch (error) {
+			throw error;
+		}
+	}
 
-        try {
-            this.CategoryProducts = await this.getProductsFromKeyword(this.word); //palabras claves
-            for (let product in this.CategoryProducts.results) {
-                this.createTrendElement(this.CategoryProducts.results[product]);
-            }
-        } catch (error) {
-            throw new Error(error);
-        }
-    }
-
-    start() {
-        myStore.getTrendProducts();
-    }
+	start() {
+		myStore.getTrendProducts();
+	}
 }
 
 let myStore = new Store(defaultSearch);
 myStore.start();
 
 function setSearchValue(store) {
-    let search = document.getElementById('Search');
+	let search = document.getElementById('Search');
 
-    try {
-        getSearchProducts(store, search.value);
-    } catch (error) {
-        throw new Error(error);
-    }
+	try {
+		getSearchProducts(store, search.value);
+	} catch (error) {
+		throw error;
+	}
 }
 
 function getSearchProducts(store, keywords) {
-    store.word = keywords;
-    try {
-        store.getTrendProducts();
-    } catch (error) {
-        throw new Error(error);
-    }
+	store.word = keywords;
+	try {
+		store.getTrendProducts();
+	} catch (error) {
+		throw error;
+	}
 }
-
-
-
