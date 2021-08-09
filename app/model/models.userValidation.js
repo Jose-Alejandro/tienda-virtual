b4016f9bfccefeb4a19295bd562e7e72adb80afb
alejandro <jose.alejandro.esquivel@gmail.com>
2021-08-09 10:19:05 -0500

    add spaces

--- app/model/models.userValidation.js
+++ app/model/models.userValidation.js
@@ -9,13 +9,13 @@ module.exports = {
 		}).with('userName', 'password'),
 	registerModel: Joi.object().keys(
 		{
-			names: Joi.string().alphanum().min(6).max(50),
+			names: Joi.string().regex(/^[a-zA-Z\s]*$/).min(6).required(),
 			last_names: Joi.string().regex(/^[a-zA-Z\s]*$/).min(6).required(),
 			email: Joi.string().email().required(),
 			userName: Joi.string().alphanum().min(6).max(16).required(),
 			phone_number: Joi.string().length(10).required(),
-			billing_address: Joi.string().alphanum().min(6).max(16).required(),
-			delivery_address: Joi.string().alphanum().min(6).max(16).required(),
+			billing_address: Joi.string().regex(/^[a-zA-Z0-9\s]*$/).min(6).required(),
+			delivery_address: Joi.string().regex(/^[a-zA-Z0-9\s]*$/).min(6).required(),
 			password: Joi.string().regex(/^[a-zA-Z0-9]{4,16}$/).min(4).required()
 		}).with('userName', 'password')
 };
